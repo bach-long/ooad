6a176015ef57a0168457b61eb004284734f1b5f5
bach-long <nguyenbach19122002@gmail.com>
2023-04-08 00:38:14 +0700

    add trigger for profile

--- be/app/Models/Profile.php
+++ be/app/Models/Profile.php
@@ -7,6 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    public static function boot()
+    {
+        parent::boot();
+
+        static::updated(function ($model) {
+            if ($model->role != 1) {
+                $user = User::where('id', $model->applier_id)->first();
+                if ($user) {
+                    $user->update([
+                        'birth_year' => $model->birth_year, 
+                        'fullname' => $model->fullname, 
+                        'gender' => (int)$model->gender + 2, 
+                        'email' => $model->email
+                    ]);
+                }
+            }
+        });
+    }
     use HasFactory;
     protected $fillable = [
         'email',
@@ -20,48 +38,57 @@ class Profile extends Model
         'description',
         'year_of_experience',
         'address_id',
-        'desire'
+        'desire',
     ];
 
     protected $primarykey = 'id';
 
     public $incrementing = true;
 
-    public function category() {
+    public function category()
+    {
         return $this->belongsTo(Category::class, 'category_id', 'id');
     }
 
-    public function applier() {
+    public function applier()
+    {
         return $this->belongsTo(User::class, 'applier_id', 'id');
     }
 
-    public function projects () {
+    public function projects()
+    {
         return $this->hasMany(Project::class, 'profile_id', 'id');
     }
 
-    public function expDetail () {
+    public function expDetail()
+    {
         return $this->hasMany(EXPdetail::class, 'profile_id', 'id');
     }
 
-    public function address () {
+    public function address()
+    {
         return $this->belongsTo(Address::class, 'address_id', 'id');
     }
 
-    public function expYear () {
+    public function expYear()
+    {
         return $this->belongsTo(Exp::class, 'year_of_experience', 'id');
     }
 
-    public function level () {
+    public function level()
+    {
         return $this->belongsTo(Level::class, 'level_id', 'id');
     }
 
-    public function skills () {
+    public function skills()
+    {
         return $this->belongsToMany(Skill::class, 'skill_profile', 'profile_id', 'skill_id')
-        ->using(Skill_profile::class)->withPivot('id', 'profile_id', 'skill_id')->withTimestamps();
+            ->using(Skill_profile::class)->withPivot('id', 'profile_id', 'skill_id')->withTimestamps();
     }
 
-    public function workablePlaces () {
+    public function workablePlaces()
+    {
         return $this->belongsToMany(Address::class, 'work_address', 'profile_id', 'workable_place_id')
-        ->using(Work_address::class)->withPivot('id', 'profile_id', 'workable_place_id')->withTimestamps();
+            ->using(Work_address::class)->withPivot('id', 'profile_id', 'workable_place_id')->withTimestamps();
     }
 }
